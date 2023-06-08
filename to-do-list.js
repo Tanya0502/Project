33a3caf623dbe1703039196c9900219e7b107d4d
Tanya <lisotanya.2@gmail.com>
2023-06-09 00:06:39 +0200

    Add function addWednesday-addSunday

--- to-do-list.js
+++ to-do-list.js
@@ -1,10 +1,70 @@
- var list = document.getElementById("Mondaylist")
+function addMonday() {
+       var list = document.getElementById("Mondaylist")
+       var inputText = document.getElementById("monday-input").value
+       var listItem = document.createElement("li")
 
- function add() {
-        var inputText = document.getElementById("monday-input").value
-        var listItem = document.createElement("li")
-
-        listItem.innerHTML = inputText
-        document.getElementById("monday-input").value =""
-        list.appendChild(listItem) 
+       listItem.innerHTML = inputText
+       document.getElementById("monday-input").value =""
+       list.appendChild(listItem) 
 }
+
+function addTuesday() {
+       var list = document.getElementById("Tuesdaylist")
+       var inputText = document.getElementById("tuesday-input").value
+       var listItem = document.createElement("li")
+
+       listItem.innerHTML = inputText
+       document.getElementById("tuesday-input").value =""
+       list.appendChild(listItem) 
+}
+
+function addWednesday() {
+       var list = document.getElementById("Wednesdaylist")
+       var inputText = document.getElementById("wednesday-input").value
+       var listItem = document.createElement("li")
+
+       listItem.innerHTML = inputText
+       document.getElementById("wednesday-input").value =""
+       list.appendChild(listItem) 
+}
+
+function addThursday() {
+       var list = document.getElementById("Thursdaylist")
+       var inputText = document.getElementById("thursday-input").value
+       var listItem = document.createElement("li")
+
+       listItem.innerHTML = inputText
+       document.getElementById("thursday-input").value =""
+       list.appendChild(listItem) 
+}
+
+function addFreiday() {
+       var list = document.getElementById("Freidaylist")
+       var inputText = document.getElementById("freiday-input").value
+       var listItem = document.createElement("li")
+
+       listItem.innerHTML = inputText
+       document.getElementById("freiday-input").value =""
+       list.appendChild(listItem) 
+}
+
+function addSaturday() {
+       var list = document.getElementById("Saturdaylist")
+       var inputText = document.getElementById("saturday-input").value
+       var listItem = document.createElement("li")
+
+       listItem.innerHTML = inputText
+       document.getElementById("saturday-input").value =""
+       list.appendChild(listItem) 
+}
+
+function addSunday() {
+       var list = document.getElementById("Sundaylist")
+       var inputText = document.getElementById("sunday-input").value
+       var listItem = document.createElement("li")
+
+       listItem.innerHTML = inputText
+       document.getElementById("sunday-input").value =""
+       list.appendChild(listItem) 
+}
+
